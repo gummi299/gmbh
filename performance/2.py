4ca24df9b6eb781e92443fb0bf87964c58721b22
gummi299 <84054274+gummi299@users.noreply.github.com>
2021-05-12 12:51:58 +0900

    Update 2.py
    No tab

--- performance/2.py
+++ performance/2.py
@@ -4,7 +4,7 @@ array = input().split()
 max = int(array[0])
 
 for i in range(n):
-	if int(array[i]) > max:
-		max = int(array[i])
+    if int(array[i]) > max:
+        max = int(array[i])
 		
 print(max)
